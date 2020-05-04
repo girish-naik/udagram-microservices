1c594879b90df3102378cb233371e6dd61c6ed5f
mr-obiwankenobi <crazygirishnaik@gmail.com>
2020-05-04 09:32:21 +0530

    Adding scripts for infra procurement and k8s install on the procured infra.

--- udacity-c3-deployment/k8s/kube_deploy.sh
+++ udacity-c3-deployment/k8s/kube_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 echo "Setting up files"
- sed -i "s/___INSERT_AWS_CREDENTIALS_FILE__BASE64____/$AWS_CREDENTIALS/g" aws-secret.yaml
- sed -i "s/___INSERT_POSTGRESS_USERNAME__BASE64___/${POSTGRESS_USERNAME}/g" env-secret.yaml
- sed -i "s/___INSERT_POSTGRESS_PASSWORD__BASE64___/${POSTGRESS_PASSWORD}/g" env-secret.yaml
- sed -i "s/___INSERT_AWS_BUCKET___/${AWS_BUCKET}/g" env-configmap.yaml
- sed -i "s/___INSERT_AWS_PROFILE___/${AWS_PROFILE}/g" env-configmap.yaml
- sed -i "s/___INSERT_AWS_REGION___/${AWS_REGION}/g" env-configmap.yaml
- sed -i "s/___INSERT_JWT_SECRET___/${JWT_SECRET}/g" env-configmap.yaml
- sed -i "s/___INSERT_POSTGRESS_DB___/${POSTGRESS_DB}/g" env-configmap.yaml
- sed -i "s/___INSERT_POSTGRESS_HOST___/${POSTGRESS_HOST}/g" env-configmap.yaml
+ sed -i '' -e "s/___INSERT_AWS_CREDENTIALS_FILE__BASE64____/${AWS_CREDENTIALS}/g" aws-secret.yaml
+ sed -i '' -e "s/___INSERT_POSTGRESS_USERNAME__BASE64___/${POSTGRESS_USERNAME}/g" env-secret.yaml
+ sed -i '' -e "s/___INSERT_POSTGRESS_PASSWORD__BASE64___/${POSTGRESS_PASSWORD}/g" env-secret.yaml
+ sed -i '' -e "s/___INSERT_AWS_BUCKET___/${AWS_BUCKET}/g" env-configmap.yaml
+ sed -i '' -e "s/___INSERT_AWS_PROFILE___/${AWS_PROFILE}/g" env-configmap.yaml
+ sed -i '' -e "s/___INSERT_AWS_REGION___/${AWS_REGION}/g" env-configmap.yaml
+ sed -i '' -e "s/___INSERT_JWT_SECRET___/${JWT_SECRET}/g" env-configmap.yaml
+ sed -i '' -e "s/___INSERT_POSTGRESS_DB___/${POSTGRESS_DB}/g" env-configmap.yaml
+ sed -i '' -e "s/___INSERT_POSTGRESS_HOST___/${POSTGRESS_HOST}/g" env-configmap.yaml
 
 echo "Deploying secrets"
 kubectl delete secret env-secret
